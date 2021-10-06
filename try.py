3422ee575e4c7baa3a073b90591b57300eba5af5
Jorge Eduardo Ponce Leyva <jorge.ponce@ift.org.mx>
2021-10-06 02:52:11 -0500

    delete

--- try.py
+++
@@ -1,9 +0,0 @@
-name = "Sal Hobbi" # String
-Salary = 123.75 # Float
-age = 20 # Integer
-message = "" # String
-salary = 89.50 #Interger
-isweatherWarm = True # Boleean
-
-print("my name is " + name) # concatenate
-print("my age is " + str(age)) # print only strings
